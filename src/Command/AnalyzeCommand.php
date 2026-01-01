079beb69c7a35a9ea8c3d6d745c5e406fd869ca0
Tomas Votruba <tomas.vot@gmail.com>
2026-01-01 22:13:32 +0100

    cs

--- src/Command/AnalyzeCommand.php
+++ src/Command/AnalyzeCommand.php
@@ -101,7 +101,12 @@ final readonly class AnalyzeCommand implements CommandInterface
         $i = 1;
         foreach ($allRuleErrors as $allRuleError) {
             $this->outputPrinter->writeln(
-                sprintf('<fg=yellow>%d) %s</> [id: <fg=cyan>%s</>]', $i, $allRuleError->getMessage(), $allRuleError->getIdentifier()),
+                sprintf(
+                    '<fg=yellow>%d) %s</> [id: <fg=cyan>%s</>]',
+                    $i,
+                    $allRuleError->getMessage(),
+                    $allRuleError->getIdentifier()
+                ),
                 1
             );
 
