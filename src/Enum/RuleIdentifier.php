34ae170d026a84cae434d035264132b644825e35
Tomas Votruba <tomas.vot@gmail.com>
2026-01-22 16:14:02 +0100

    add missing context definitions

--- src/Enum/RuleIdentifier.php
+++ src/Enum/RuleIdentifier.php
@@ -15,4 +15,6 @@ final class RuleIdentifier
     public const string UNUSED_DEFINITIONS = 'unused-definitions';
 
     public const string REDUNDANT_REGEX_DEFINITION = 'redundant-regex-definition';
+
+    public const string MISSING_CONTEXT_DEFINITIONS = 'missing-context-definitions';
 }
---
+++ stubs/Behat/Behat/Config.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Behat\Config;
+
+final class Config
+{
+    public function toArray(): array
+    {
+        return [];
+    }
+}
