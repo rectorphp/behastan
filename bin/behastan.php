173fb9aa096768084b12cddf7a4e927b16a9c730
Tomas Votruba <tomas.vot@gmail.com>
2026-06-05 23:41:04 +0200

    tokens

--- bin/behastan.php
+++ bin/behastan.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Entropy\Console\ConsoleApplication;
 use Rector\Behastan\DependencyInjection\BehastanContainerFactory;
 
 $possibleAutoloadPaths = [
@@ -25,8 +26,23 @@ if (file_exists($scoperAutoloadFilepath)) {
     require_once $scoperAutoloadFilepath;
 }
 
+// the released tool is downgraded to PHP 7.2, but the bundled nikic/php-parser
+// references PHP 7.4 token constants directly - define them to avoid fatal errors
+// (see https://github.com/easy-coding-standard/ecs/blob/main/bin/ecs.php for the same approach)
+if (! defined('T_FN')) {
+    define('T_FN', 5025);
+}
+
+if (! defined('T_COALESCE_EQUAL')) {
+    define('T_COALESCE_EQUAL', 5030);
+}
+
+if (! defined('T_BAD_CHARACTER')) {
+    define('T_BAD_CHARACTER', 5035);
+}
+
 $container = BehastanContainerFactory::create();
-$consoleApplication = $container->make(\Entropy\Console\ConsoleApplication::class);
+$consoleApplication = $container->make(ConsoleApplication::class);
 
 $exitCode = $consoleApplication->run($argv);
 exit($exitCode);
